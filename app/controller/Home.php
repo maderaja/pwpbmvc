e4f5edbd756ababf7d42ae91c93bd0ef08dac542
RajaMahendra <maderajam07@gmail.com>
2021-10-12 09:42:05 +0800

    menambah Routing

---
+++ app/controller/Home.php
@@ -0,0 +1,14 @@
+<?php
+
+class Home extends Controller
+{
+  public function index()
+  {
+    echo "Ini Halaman Home/index";
+  }
+
+  public function coba()
+  {
+    echo "Ini Halaman Home/coba";
+  }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,54 @@
 
 class App
 {
+  protected $controller = 'Home';
+  protected $method = 'index';
+  protected $params = [];
+
   public function __construct()
   {
-    echo "Web MVC Siap Dijalankan!";
+    $url = $this->parseURL();
+
+    // Controller
+    if ($url == NULL) {
+      $url = [$this->controller];
+    }
+
+    if (file_exists('../app/controller/' . $url[0] . '.php')) {
+      $this->controller = $url[0];
+      unset($url[0]);
+    }
+
+    require_once '../app/controller/' . $this->controller . '.php';
+    $this->controller = new $this->controller;
+
+
+    // Method
+    if (isset($url[1])) {
+      if (method_exists($this->controller, $url[1])) {
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+
+
+    // Params
+    if (!empty($url)) {
+      $this->params = array_values($url);
+    }
+
+    // jalankan controller & method, serta kirimkan params jika ada
+    call_user_func_array([$this->controller, $this->method], $this->params);
+  }
+
+  public function parseURL()
+  {
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+
+      return $url;
+    }
   }
 }
